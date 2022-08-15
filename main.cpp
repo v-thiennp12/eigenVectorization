201e89477dd4da9cc0214cea45b605fc9b9f6823
v-thiennp12 <nguyenrobot@gmail.com>
2022-08-15 12:01:00 +0700

    redo GLM

--- main.cpp
+++ main.cpp
@@ -3,17 +3,18 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <iostream>
+#include "glm.hpp" // glm::vec3
 
 using namespace std;
 using Eigen::MatrixXf;
 using Eigen::VectorXf;
 
-MatrixXf    ConvTrans_Vector2DEigen(const vector<vector<float>> &vector2D);
-MatrixXf    Conv_Vector2DEigen(const vector<vector<float>> &vector2D);
-MatrixXf    Conv_Vector9fEigen3f(const vector<float> &vector9f);
+MatrixXf    Conv_Vertices2Eigen(const vector<glm::vec3> &);
+MatrixXf    Conv_Vector9x1fEigen3x3f(const vector<float> &vector9f);
 MatrixXf    Conv_Vector1DEigenVec(const vector<float> &vector1D);
 void        printEigen2D(const Eigen::MatrixXf &eigenMat);
 
+//history : redo the GLM processing
 struct camIFs_strct {
     vector<float> matrixD       = {1,2,3,4};
     vector<float> matrixK       = {1,2,3,4};
@@ -22,37 +23,16 @@ struct camIFs_strct {
 };
 
 int main () {
-    //     std::vector<std::vector<float>> vector2D{
-    //                                             {1,2,3,4,2,3,4},
-    //                                             {3,4,5,6,4,5,6},
-    //                                             {9,10,11,12,10,11,12}
-    //                                             };
-    //     std::vector<float> vector9f = {1,2,3,4,5,6,7,8,9};
-
-    // // test ConvTrans_Vector2DEigen
-    //     MatrixXf eigen2D;
-    //     eigen2D = ConvTrans_Vector2DEigen(vector2D);
-    //             printEigen2D(eigen2D);
-
-    // // test Conv_Vector2DEigen
-    //     eigen2D = Conv_Vector2DEigen(vector2D);
-    //             printEigen2D(eigen2D);
-
-    // // test Conv_Vector9fEigen3f
-    //     MatrixXf eigen3f;
-    //     eigen3f = Conv_Vector9fEigen3f(vector9f);
-    //             // printEigen2D(eigen3f);
-
-    //------------------------
+    //---------------------------------------------
     const int playGroundVerticesSize  = 5;
-    vector<vector<float>>       playGroundVertices{
-                                        {1,2,3},
-                                        {3,4,5},
-                                        {5,6,7},
-                                        {7,8,9},
-                                        {9,10,11}
-                                     };
-    int i=0;    
+    vector<glm::vec3>       playGroundVertices{
+                                    glm::vec3(1,2,3),
+                                    glm::vec3(3,4,5),
+                                    glm::vec3(5,6,7),
+                                    glm::vec3(7,8,9),
+                                    glm::vec3(9,10,11)
+                                };
+    int i   =   0;
 
     vector<camIFs_strct> camIFs;
     camIFs.push_back(camIFs_strct());
@@ -63,19 +43,19 @@ int main () {
     float RAW_IMG_HEIGHT            = 600;
     float RAW_IMG_WIDTH             = 800;
     
-    vector<vector<float>> uvaOuts;
-    // Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+    //---------------------------------------------
+    vector<glm::vec3> uvaOuts;
 
     Eigen::MatrixXf 				eig_playGroundVertices;
     Eigen::Matrix3f 				eig_matrixR;
     Eigen::MatrixXf					eig_vectT;    
 
-        eig_matrixR 					= Conv_Vector9fEigen3f(camIFs[i].matrixR);
-		eig_vectT 						= Conv_Vector1DEigenVec(camIFs[i].vectT).replicate(1, playGroundVerticesSize);
-        eig_playGroundVertices          = ConvTrans_Vector2DEigen(playGroundVertices);
-        printEigen2D(eig_matrixR);
-        printEigen2D(eig_vectT);
-        printEigen2D(eig_playGroundVertices);        
+        eig_matrixR 				= Conv_Vector9x1fEigen3x3f(camIFs[i].matrixR);
+		eig_vectT 					= Conv_Vector1DEigenVec(camIFs[i].vectT).replicate(1, playGroundVerticesSize);
+        eig_playGroundVertices      = Conv_Vertices2Eigen(playGroundVertices);
+            printEigen2D(eig_matrixR);
+            printEigen2D(eig_vectT);
+            // printEigen2D(eig_playGroundVertices);
     
     //vecPointTransformed
     Eigen::Matrix<float,3,playGroundVerticesSize,Eigen::RowMajor>  	eig_vecWorldPosRotate;
@@ -179,21 +159,18 @@ int main () {
 			
             printEigen2D(eig_UVA);
 
-		for (int j = 0; j < playGroundVerticesSize; ++j ){
-            // std::vector<float> v3 (eig_UVA.row(j).data(),  eig_UVA.row(j).data() + eig_UVA.row(j).size() );
-            std::vector<float> v3 (&eig_UVA.row(j)(0), &eig_UVA.row(j)(0) + eig_UVA.row(j).cols()*eig_UVA.row(j).rows());
-            // std::vector<float> v3 = {eig_UVA.row(j)(0), eig_UVA.row(j)(1), eig_UVA.row(j)(2)};
-            uvaOuts.push_back(v3);
+		for (int k = 0; k < playGroundVerticesSize; ++k ){
+            uvaOuts.push_back(glm::vec3(eig_UVA(0, k), eig_UVA(1, k), eig_UVA(2, k)));
 		}
 
         int j = 0;
-        cout << "mem adr " << &eig_UVA.row(j)(0) << " mem adr " <<  &eig_UVA.row(j)(1) <<  " mem adr " << &eig_UVA.row(j)(2) << endl;
-        cout << "mem adr " << &eig_UVA.row(j)(0) <<  " mem adr " << &eig_UVA.row(j)(0) + eig_UVA.row(j).cols()*eig_UVA.row(j).rows() << endl;
+        cout << "mem adr " << &eig_UVA.row(j)(0) << " mem adr " << &eig_UVA.row(j)(1) <<  " mem adr " << &eig_UVA.row(j)(2) << " mem adr " << &eig_UVA.row(j)(3) << endl;
+        cout << "mem adr " << &eig_UVA.row(j)(0) << " mem adr " << &eig_UVA.row(j)(0) + eig_UVA.row(j).cols()*eig_UVA.row(j).rows() << endl;
 
         // check uvaOuts
         cout << "uvaOuts" << endl;
 		for (int j = 0; j < uvaOuts.size(); j++ ){
-            for (int k = 0; k < uvaOuts[j].size(); k++ ){
+            for (int k = 0; k < 3; k++ ){
                 cout << uvaOuts[j][k] << " ";
             }
             cout << endl;
@@ -213,61 +190,33 @@ void printEigen2D(const Eigen::MatrixXf &eigen2D) {
     }
 }
 
-MatrixXf ConvTrans_Vector2DEigen(const vector<vector<float>> &vector2D) {
-    // vector2D[i][j] >> eigen2D(j, i)
-        // convert and transpose in the same time
+MatrixXf Conv_Vertices2Eigen(const vector<glm::vec3> &vertices) {
+    // vertices[i][j] >> eigen2D(j, i)
+        // convert vector of vertices (vec3) into 3x_ eigen 2D RowMajor
 
-    MatrixXf eigen2D = Eigen::MatrixXf::Constant(vector2D[0].size(), vector2D.size(), 0.0f);
+    Eigen::Matrix<float, 3, Eigen::Dynamic, Eigen::RowMajor> eigen2D = Eigen::MatrixXf::Constant(3, vertices.size(), 0.0f);
 
-    for (int i{0}; i < vector2D.size(); ++i) {
-        eigen2D.col(i) = Eigen::VectorXf::Map(&vector2D[i][0], vector2D[i].size());
+    for (int i{0}; i < vertices.size(); ++i) {
+        eigen2D(0,i)  = vertices[i][0];
+        eigen2D(1,i)  = vertices[i][1];
+        eigen2D(2,i)  = vertices[i][2];
     }
-    
     return eigen2D;
 }
 
-MatrixXf Conv_Vector2DEigen(const vector<vector<float>> &vector2D) {
-    // vector2D[i][j] >> eigen2D(i, j)
-        // convert
-
-    MatrixXf eigen2D = Eigen::MatrixXf::Constant(vector2D.size(), vector2D[0].size(), 0.0f);
-
-    for (int i{0}; i < vector2D.size(); ++i) {
-        eigen2D.row(i) = Eigen::VectorXf::Map(&vector2D[i][0], vector2D[i].size());
-    }
-    
-    return eigen2D;
-}
-
-MatrixXf Conv_Vector9fEigen3f(const vector<float> &vector9f) {
-    // vector2D[i] >> eigen2D(i, j)
+MatrixXf Conv_Vector9x1fEigen3x3f(const vector<float> &vector9f) {
+    // vector1D[k] >> eigen2D(i, j)
         // convert 9 element vector 1D into 3x3 eigen, row-major
-            // eigen3f  = Eigen::Map<Eigen::Matrix<float, 3, 3>> (&vector9f);
-            // eigen3f  = Eigen::MatrixXf::Map(&vector9f, 3, 3);
 
-    MatrixXf eigen3f    = Eigen::MatrixXf::Constant(3, 3, 0.0f);
-    eigen3f.row(0)      = Eigen::VectorXf::Map(&vector9f[0], 3);
-    eigen3f.row(1)      = Eigen::VectorXf::Map(&vector9f[3], 3);
-    eigen3f.row(2)      = Eigen::VectorXf::Map(&vector9f[6], 3);
+    MatrixXf eigen3x3f          = Eigen::MatrixXf::Constant(3, 3, 0.0f);
+        eigen3x3f.row(0)        = Eigen::VectorXf::Map(&vector9f[0], 3);
+        eigen3x3f.row(1)        = Eigen::VectorXf::Map(&vector9f[3], 3);
+        eigen3x3f.row(2)        = Eigen::VectorXf::Map(&vector9f[6], 3);
 
-    return eigen3f;
+    return eigen3x3f;
 }
 
 MatrixXf Conv_Vector1DEigenVec(const vector<float> &vector1D) {
-    // vector2D[i] >> eigen2D(i, j)
-        // convert 9 element vector 1D into 3x3 eigen, row-major
-            // eigen3f  = Eigen::Map<Eigen::Matrix<float, 3, 3>> (&vector9f);
-            // eigen3f  = Eigen::MatrixXf::Map(&vector9f, 3, 3);
-
-    // MatrixXf eigenVec;
-    // eigen1D = Eigen::VectorXf::Map(&vector1D[0], vector1D.size());
-    // eigenVec = Eigen::Map<Eigen::VectorXf>(&vector1D[0], vector1D.size());
-
-        // float* ptr_data = &v1[0];
-        // Eigen::VectorXf v2 = Eigen::Map<Eigen::VectorXf, Eigen::Unaligned>(v1.data(), v1.size());
-
-        // Eigen::VectorXf v4 = Eigen::Map<Eigen::VectorXf, Eigen::Unaligned>(ptr_data, v1.size());
     Eigen::MatrixXf eigenVec    = Eigen::VectorXf::Map(&vector1D[0], vector1D.size());
-
     return eigenVec;
 }
